c42802bd38b7fcb322f72634317692d0d45ef0be
André Junior <andre.junior@olx.com>
2022-03-20 12:47:08 +0000

    Change logic to find shortest

--- src/utils/prefix_handler.py
+++ src/utils/prefix_handler.py
@@ -5,6 +5,7 @@ import pandas as pd
 from bisect import bisect_left
 from dataclasses import dataclass
 from typing import Dict, List
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "utils"))
 
 
@@ -13,7 +14,7 @@ class PrefixHandler:
     """Class for handle prefix search."""
 
     def geohash_dict_to_df(self, geohash_short_prefix: Dict) -> pd.DataFrame:
-        """ Function to convert the dictionary with geohash enriched data.
+        """Function to convert the dictionary with geohash enriched data.
 
         Args:
             geohash_short_prefix (Dict): Dict with data to be converted
@@ -21,7 +22,10 @@ class PrefixHandler:
         Returns:
             pd.DataFrame: Dataframe with geohash and shortest prefix
         """
-        geohash_lookup = [{"geohash": key, "unique_prefix": value} for key, value in geohash_short_prefix.items()]
+        geohash_lookup = [
+            {"geohash": key, "unique_prefix": value}
+            for key, value in geohash_short_prefix.items()
+        ]
         geohash_enriched_df = pd.DataFrame(geohash_lookup)
         return geohash_enriched_df
 
@@ -36,7 +40,9 @@ class PrefixHandler:
             bool: Return if prefix exists or not
         """
         try:
-            return geohash_list[bisect_left(geohash_list, geohash_prefix)].startswith(geohash_prefix)
+            return geohash_list[bisect_left(geohash_list, geohash_prefix)].startswith(
+                geohash_prefix
+            )
         except IndexError:
             return False
 
@@ -50,14 +56,14 @@ class PrefixHandler:
             List: List with all possible geohash prefixes
         """
         geohash_prefixes_list = []
-        for letter in range(1, len(geohash_code)+1):
+        for letter in range(1, len(geohash_code) + 1):
             prefix = geohash_code[:letter]
             geohash_prefixes_list.append(prefix)
         return geohash_prefixes_list
 
-    def get_shortest_prefix(self, geohash_prefixes_map: List,
-                            geohash_list: List,
-                            geohash_code: str) -> Dict:
+    def get_shortest_prefix(
+        self, geohash_prefixes_map: List, geohash_list: List, geohash_code: str
+    ) -> Dict:
         """Find the shortest prefix to a respective Geohash
 
         Args:
@@ -69,7 +75,9 @@ class PrefixHandler:
             Dict: Returns a Map with Geohash and shortest prefix
         """
         geohash_idx = geohash_list.index(geohash_code)
-        geohash_aux_list = geohash_list.copy()
+        geohash_left = geohash_idx - 1 if geohash_idx != 0 else 0
+        geohash_right = geohash_idx + 2
+        geohash_aux_list = geohash_list[geohash_left:geohash_right]
         geohash_aux_list.pop(geohash_aux_list.index(geohash_code))
         geohash_output_map = {}
 
@@ -81,7 +89,7 @@ class PrefixHandler:
                 return geohash_output_map
 
     def geohash_mapper(self, geohash_list: List) -> pd.DataFrame:
-        """ Call functions responsible for enrich GeoHash with shortest prefix
+        """Call functions responsible for enrich GeoHash with shortest prefix
 
         Args:
             geohash_list (List): List with all geohashes
@@ -93,9 +101,9 @@ class PrefixHandler:
 
         for geohash_code in geohash_list:
             geohash_prefixes_map = self.get_geohash_prefixes(geohash_code)
-            geohash_shortest_map = self.get_shortest_prefix(geohash_prefixes_map,
-                                                            geohash_list,
-                                                            geohash_code)
+            geohash_shortest_map = self.get_shortest_prefix(
+                geohash_prefixes_map, geohash_list, geohash_code
+            )
 
             prefix_shortest_list[geohash_code] = geohash_shortest_map[geohash_code]
         return self.geohash_dict_to_df(prefix_shortest_list)
